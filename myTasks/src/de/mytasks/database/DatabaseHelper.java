ad56dbe4b70a0e6665a5bf4dfe40e2fcf631e2a2
Jürgen Biedron <biju1012@hs-karlsruhe.de>
2014-04-25 13:56:22 +0200

    anpassung

--- myTasks/src/de/mytasks/database/DatabaseHelper.java
+++ myTasks/src/de/mytasks/database/DatabaseHelper.java
@@ -31,7 +31,7 @@ public class DatabaseHelper extends OrmLiteSqliteOpenHelper {
 	private static final String DATABASE_NAME = "taskDB.db";
 	// any time you make changes to your database objects, you may have to
 	// increase the database version
-	private static final int DATABASE_VERSION = 3;
+	private static final int DATABASE_VERSION = 4;
 
 	private DatabaseHelper databaseHelper = null;
 
