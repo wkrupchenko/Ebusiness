e8f64bcab0ce77dcfd790d04a72ff1312a65a5aa
Jürgen Biedron <biju1012@hs-karlsruhe.de>
2014-05-12 14:54:52 +0200

    TasklistActivity Anpassung: Darstellung der Tasklist. Aus JSON --> Java Objekt

--- myTasks/src/de/mytasks/activities/TasklistActivity.java
+++ myTasks/src/de/mytasks/activities/TasklistActivity.java
@@ -1,15 +1,31 @@
 package de.mytasks.activities;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import com.google.gson.Gson;
 import com.j256.ormlite.android.apptools.OrmLiteBaseActivity;
 
 import de.mytasks.R;
 import de.mytasks.database.DatabaseHelper;
+import de.mytasks.domain.Tasklist;
+import de.mytasks.service.SimpleHttpClient;
 
 public class TasklistActivity extends OrmLiteBaseActivity<DatabaseHelper> {
 
@@ -18,7 +34,12 @@ public class TasklistActivity extends OrmLiteBaseActivity<DatabaseHelper> {
 	private Button settings;
 	private Button update;
 	private DatabaseHelper databaseHelper;
-	private ListView tasklistOverwiewWindow;
+	private ListView tasklistOverviewWindow;
+	private String resp;
+	private ArrayList<String> list = new ArrayList<String>();
+	private static final String TAG = "TasklistActivity";
+	private List<Tasklist> allTasklists = new ArrayList<Tasklist>();
+	private ArrayAdapter<String> adapter;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -28,8 +49,178 @@ public class TasklistActivity extends OrmLiteBaseActivity<DatabaseHelper> {
 		settings = (Button) findViewById(R.id.shareButton);
 		update = (Button) findViewById(R.id.updateButton);
 		taskListName = (EditText) findViewById(R.id.newTasklistNameTitle);
-		tasklistOverwiewWindow = (ListView) findViewById(R.id.tasklistOverwiewWindow);
+		tasklistOverviewWindow = (ListView) findViewById(R.id.tasklistOverviewWindow);
+		update.setOnClickListener(myhandler1);
+		creatNewTask.setOnClickListener(myhandler2);
+		
+//		final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+//				android.R.layout.simple_list_item_1, list);
+		adapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, list);
+
+		tasklistOverviewWindow.setAdapter(adapter);
+		list.add("Test");
+	
 	}
+	
+	View.OnClickListener myhandler2 = new View.OnClickListener() {
+	    public void onClick(View v) {
+	    	
+	    	new Thread(new Runnable(){
+	    		
+		    	@Override
+		    	public void run() {
+		    		ArrayList<NameValuePair> postParameters = new ArrayList<NameValuePair>();
+		    		postParameters.add(new BasicNameValuePair("username","pipi"));
+		    		postParameters.add(new BasicNameValuePair("password","qqq"));
+		    		postParameters.add(new BasicNameValuePair("tasklistname",taskListName.getText().toString()));
+		    	    postParameters.add(new BasicNameValuePair("ownerid","14"));
+		    	    
+		    	    String response = null;
+		    	      try {
+//		    	       response = SimpleHttpClient.executeHttpPost("http://10.0.2.2:8080/mytasksRegister/show", postParameters);
+//		    	       response = SimpleHttpClient.executeHttpPost("http://10.0.2.2:8080/mytasksService/register", postParameters);
+//		    	       response = SimpleHttpClient.executeHttpPost("http://iwi-w-eb03:8080/mytasksService/register", postParameters);
+		    	       response = SimpleHttpClient.executeHttpPost("http://www.iwi.hs-karlsruhe.de/eb03/createTasklist", postParameters);
+		    	       String res = response.toString();
+		    	       resp = res;
+		    	}
+		    	      catch (Exception e) {
+		    	          e.printStackTrace();
+		    	          Toast.makeText(getApplicationContext(), e.getMessage(),Toast.LENGTH_SHORT).show();
+		    	      }
+		    	} 
+	    	}).start();
+	    	
+	    	try {
+
+	    	    /** wait a second to get response from server */
+	    	    Thread.sleep(1000);
+	    	    /** Inside the new thread we cannot update the main thread
+	    	    So updating the main thread outside the new thread */
+	    	    	if (null != resp && !resp.isEmpty()) {
+	    	    		 boolean check = resp.contains("Created");	    	    		  
+	    	    	       if (check == true) {
+	    	    	    	   Toast.makeText(getApplicationContext(), "New Tasklist successfully created",Toast.LENGTH_LONG).show();
+	    	    	    	   Intent it = new Intent(getApplicationContext(),TasklistActivity.class);
+	    	    	    	   startActivity(it);
+	    	    	       } 
+	    	    	}
+	    	    	
+	    	    	else {
+	    	    			Toast.makeText(getApplicationContext(), "something went wrong, check your connection",Toast.LENGTH_SHORT).show();
+	    	    	} 
+	    	            
+	    	   
+	    	    } catch (Exception e) {
+	    	    	
+	    	 }
+	    }
+	  };
+	  
+	  View.OnClickListener myhandler1 = new View.OnClickListener() {
+		    public void onClick(View v) {
+		    	
+		    	new Thread(new Runnable(){
+		    		
+			    	@Override
+			    	public void run() {
+			    		ArrayList<NameValuePair> postParameters = new ArrayList<NameValuePair>();
+			    		postParameters.add(new BasicNameValuePair("username","pipi"));
+			    		postParameters.add(new BasicNameValuePair("password","qqq"));
+			    	    postParameters.add(new BasicNameValuePair("userid","14"));
+			    	    
+			    	    String response = null;
+			    	      try {
+//			    	       response = SimpleHttpClient.executeHttpPost("http://10.0.2.2:8080/mytasksRegister/show", postParameters);
+//			    	       response = SimpleHttpClient.executeHttpPost("http://10.0.2.2:8080/mytasksService/register", postParameters);
+//			    	       response = SimpleHttpClient.executeHttpPost("http://iwi-w-eb03:8080/mytasksService/register", postParameters);
+			    	       response = SimpleHttpClient.executeHttpPost("http://www.iwi.hs-karlsruhe.de/eb03/getTasklist", postParameters);
+			    	       String res = response.toString();
+			    	       Log.v(TAG, response.toString());
+			    	       Log.v(TAG, res.toString());
+			    	       resp = res;
+			    	}
+			    	      catch (Exception e) {
+			    	          e.printStackTrace();
+			    	          Toast.makeText(getApplicationContext(), e.getMessage(),Toast.LENGTH_SHORT).show();
+			    	      }
+			    	} 
+		    	}).start();
+		    	
+		    	try {
+
+		    	    /** wait a second to get response from server */
+		    	    Thread.sleep(1000);
+		    	    
+		    	    Log.v(TAG, resp.toString());
+		    	    
+		    	    Gson gson = new Gson();
+		    	    
+		    	    JSONArray jsonArray = new JSONArray(resp);
+		    	    
+		    	    
+		    	    for (int i = 0; i < jsonArray.length(); i++) {
+		    	        JSONObject jsonObject = jsonArray.getJSONObject(i);
+		    	        Tasklist tasklist = new Tasklist();
+		    	        String TasklistId = jsonObject.getString("id");
+		    	        String TasklistName = jsonObject.getString("name");
+		    	        String TasklistOwner = jsonObject.getString("ownerId");
+		    	        tasklist.setId(Long.valueOf(TasklistId).longValue());
+		    	        tasklist.setName(TasklistName);
+		    	        tasklist.setOwnerId(Long.valueOf(TasklistOwner).longValue());
+		    	        allTasklists.add(tasklist);
+		    	        Log.i(Tasklist.class.getName(), jsonObject.getString("name"));
+		    	      }
+		    	    
+		    	    for(Tasklist t : allTasklists){
+		    	    	list.add(t.getName());
+		    	    }
+		    	    
+		    	    
+		    	    tasklistOverviewWindow.setAdapter(adapter);
+		    	    
+		    	    
+//		    	    JSONObject responseObj = null;
+//					Log.v(TAG, "JSON Object NULL");
+//		    	    /** Inside the new thread we cannot update the main thread
+//		    	    So updating the main thread outside the new thread */
+//		    	   
+//		    	    		
+//					// parse and display JSON response
+//    	    		// create JSON object from JSON string
+//					responseObj = new JSONObject(resp);
+//					Log.v(TAG, "JSON Object Not NULL");
+//					
+//					boolean success = responseObj.getBoolean("success");
+//					String s = new Boolean(success).toString();
+//					Log.v(TAG, s);
+//					if (success) {
+//						
+//						String tasklistInfo = responseObj
+//								.getString("TasklistInfo");
+//						Log.v(TAG, tasklistInfo);
+//					
+//						// create java object from the JSON object
+//						Tasklist tasklist = gson.fromJson(tasklistInfo,
+//								Tasklist.class);
+//						Log.v(TAG, tasklist.toString());
+//						
+//						String name;
+//						name = tasklist.getName();
+//						list.add(name);
+//					}
+//		    	    	
+//		    	    	else {
+//		    	    		Log.v(TAG, "leider nicht geklappt");
+//							list.add("leider nicht geklappt");
+//		    	    	} 
+		    	    } 
+		    	catch (Exception e) {
+		    	    	e.printStackTrace();
+		    	 }
+		    }
+		  };
 
 
 	@Override
--- myTasks/src/de/mytasks/domain/Tasklist.java
+++ myTasks/src/de/mytasks/domain/Tasklist.java
@@ -1,56 +1,165 @@
 package de.mytasks.domain;
 
-import com.j256.ormlite.field.DatabaseField;
-import com.j256.ormlite.table.DatabaseTable;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
-import android.app.Activity;
-import android.os.Bundle;
+public class Tasklist {
 
-@DatabaseTable(tableName = "tasklist")
-public class Tasklist extends Activity{
+	public Tasklist(String name, Long id, boolean archived) {
+		super();
+		this.name = name;
+		this.ownerId = id;
+		this.archived = archived;
+	}
 	
-	@DatabaseField(generatedId = true)
-	private Long tasklist_id;
+	public Tasklist() {
+		// TODO Auto-generated constructor stub
+	}
+
+	private Long id; 			// TL_ID
+	private String name;		// TL_NAME
+	private Long ownerId;	// OWNER_EMAIL
+	private List<Task> tasks;	
+	private boolean archived;	// ARCHIVED
+	private Date creation;		// CREATED_ON
+	private Date updated;		// UPDATED_ON
+	private String updatedBy;	// UPDATED_BY
 	
-	@DatabaseField
-	private String tasklist_title;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public boolean isArchived() {
+		return archived;
+	}
+	public void setArchived(boolean archived) {
+		this.archived = archived;
+	}
+	public Date getCreation() {
+		return creation;
+	}
+	public void setCreation(Date creation) {
+		this.creation = creation;
+	}
+	public Date getUpdated() {
+		return updated;
+	}
+	public void setUpdated(Date updated) {
+		this.updated = updated;
+	}
+	public String getUpdatedBy() {
+		return updatedBy;
+	}
+	public void setUpdatedBy(String updatedBy) {
+		this.updatedBy = updatedBy;
+	}
+	public List<Task> getTasks() {
+		return tasks;
+	}
+	public void setTasks(List<Task> tasks) {
+		this.tasks = tasks;
+	}
+	
+	public Tasklist setTask(Task task) {
+		if (tasks == null) {
+			tasks = new ArrayList<Task>();
+		}
+		tasks.add(task);
+		return this;
+	}
 	
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (archived ? 1231 : 1237);
+		result = prime * result
+				+ ((creation == null) ? 0 : creation.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((ownerId == null) ? 0 : ownerId.hashCode());
+		result = prime * result + ((tasks == null) ? 0 : tasks.hashCode());
+		result = prime * result + ((updated == null) ? 0 : updated.hashCode());
+		result = prime * result
+				+ ((updatedBy == null) ? 0 : updatedBy.hashCode());
+		return result;
 	}
-
-	public Tasklist() {
-
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Tasklist other = (Tasklist) obj;
+		if (archived != other.archived)
+			return false;
+		if (creation == null) {
+			if (other.creation != null)
+				return false;
+		} else if (!creation.equals(other.creation))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (ownerId == null) {
+			if (other.ownerId != null)
+				return false;
+		} else if (!ownerId.equals(other.ownerId))
+			return false;
+		if (tasks == null) {
+			if (other.tasks != null)
+				return false;
+		} else if (!tasks.equals(other.tasks))
+			return false;
+		if (updated == null) {
+			if (other.updated != null)
+				return false;
+		} else if (!updated.equals(other.updated))
+			return false;
+		if (updatedBy == null) {
+			if (other.updatedBy != null)
+				return false;
+		} else if (!updatedBy.equals(other.updatedBy))
+			return false;
+		return true;
 	}
-
-	public Tasklist(String tasklist_title) {
-
-		this.tasklist_title = tasklist_title;
-	}
-
-	public Long getTasklist_id() {
-		return tasklist_id;
-	}
-
-	public void setTasklist_id(Long tasklist_id) {
-		this.tasklist_id = tasklist_id;
-	}
-
-	public String getTasklist_title() {
-		return tasklist_title;
-	}
-
-	public void setTasklist_title(String tasklist_title) {
-		this.tasklist_title = tasklist_title;
-	}
-
+	
+	
 	@Override
 	public String toString() {
-		return "Tasklist [tasklist_id=" + tasklist_id + ", tasklist_title="
-				+ tasklist_title + "]";
+		return "TaskList [id=" + id + ", name=" + name + ", ownerId=" + ownerId
+				+ ", tasks=" + tasks + ", archived=" + archived + ", creation="
+				+ creation + ", updated=" + updated + ", updatedBy="
+				+ updatedBy + "]";
+	}
+
+	public Long getOwnerId() {
+		return ownerId;
+	}
+
+	public void setOwnerId(Long ownerId) {
+		this.ownerId = ownerId;
 	}
 	
-
-
+	
+	
 }
