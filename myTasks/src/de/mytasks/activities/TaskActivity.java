e0cc4b9f959516ac034782eba53f4b206ac71cf8
Jürgen Biedron <biju1012@hs-karlsruhe.de>
2014-05-16 17:46:47 +0200

    Create Task und getTasks hard-gecodet für User "pipi" und Tasklist "200006"

--- myTasks/src/de/mytasks/activities/TaskActivity.java
+++ myTasks/src/de/mytasks/activities/TaskActivity.java
@@ -1,11 +1,21 @@
 package de.mytasks.activities;
 
- 
 import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import com.google.gson.Gson;
 
 import de.mytasks.R;
 import de.mytasks.database.DatabaseHelper;
 import de.mytasks.domain.Task;
+import de.mytasks.domain.Tasklist;
+import de.mytasks.service.SimpleHttpClient;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
@@ -14,96 +24,251 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.PopupMenu;
+import android.widget.TextView;
 import android.widget.Toast;
 
 @SuppressLint("NewApi")
 public class TaskActivity extends Activity {
- EditText title;
- EditText description;
- EditText geotag; 
- ImageView picture;
- ImageButton ib;
- Bitmap bmp;
- 
- 
- /** Called when the activity is first created. */
- @Override
- public void onCreate(Bundle savedInstanceState) {
-  super.onCreate(savedInstanceState);
-  setContentView(R.layout.activity_main);
-  title = (EditText) findViewById(R.id.edit_task_title);
-  description = (EditText) findViewById(R.id.edit_task_description);
-  picture = (ImageView) findViewById(R.id.task_picture);
-  ib =  (ImageButton) findViewById(R.id.imageButton1);
-  
-  ib.setOnClickListener(viewClickListener);    
- }
- 
- public void showTask (View view) throws NullPointerException {
-	 try {
-		 		 
-	 }
-	 
-	 catch (Exception e) {
-		  AlertDialog.Builder builder = new AlertDialog.Builder(this);
-		  builder.setMessage(e.getMessage());
-		  AlertDialog alert = builder.create();
-		  alert.show();
-	 }
-	 
+	
+	private TextView tasklistTitle;
+	private EditText taskName;
+	private Button creatNewTask;
+	private ListView tasksOverviewWindow;
+	private TextView task;
+	private CheckBox tasksCheckBox;
+	private String resp;
+	private static final String TAG = "TaskActivity";
+	private ArrayList<Task> allTasks = new ArrayList<Task>();
+	private ArrayAdapter<Task> adapter;
+	
+	
+//	EditText title;
+//	EditText description;
+//	EditText geotag;
+//	ImageView picture;
+//	ImageButton ib;
+//	Bitmap bmp;
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_tasks);
+		tasklistTitle = (TextView) findViewById(R.id.textView1);
+		taskName = (EditText) findViewById(R.id.newTasklistNameTitle);
+		creatNewTask = (Button) findViewById(R.id.addTaskButton);
+		tasksOverviewWindow = (ListView) findViewById(R.id.listView1);
+//		task = (TextView) findViewById(R.id.textView2);
+		tasksCheckBox = (CheckBox) findViewById(R.id.checkBox1);
+		
+//		title = (EditText) findViewById(R.id.edit_task_title);
+//		description = (EditText) findViewById(R.id.edit_task_description);
+//		picture = (ImageView) findViewById(R.id.task_picture);
+//		ib = (ImageButton) findViewById(R.id.imageButton1);
+//
+//		ib.setOnClickListener(viewClickListener);
+		
+		Intent i = getIntent();
+		String taskName = i.getStringExtra("Test");
+		tasklistTitle.setText(taskName);
+		
+		creatNewTask.setOnClickListener(myhandler1);
+				
+		adapter = new ArrayAdapter<Task>(this,
+		        android.R.layout.simple_list_item_1, allTasks);
+		tasksOverviewWindow.setAdapter(adapter);
+		
+		getTasks();
+	}
+	
+	View.OnClickListener myhandler1 = new View.OnClickListener() {
+	    public void onClick(View v) {
+	    	
+	    	new Thread(new Runnable(){
+	    		
+		    	@Override
+		    	public void run() {
+		    		
+		    		ArrayList<NameValuePair> postParameters = new ArrayList<NameValuePair>();
+		    		postParameters.add(new BasicNameValuePair("username","pipi"));
+		    		postParameters.add(new BasicNameValuePair("password","qqq"));
+		    		postParameters.add(new BasicNameValuePair("taskname",taskName.getText().toString()));
+		    	    postParameters.add(new BasicNameValuePair("tasklistid","200006"));
+		    	    
+		    	    String response = null;
+		    	      try {
+		    	       response = SimpleHttpClient.executeHttpPost("http://www.iwi.hs-karlsruhe.de/eb03/createTask", postParameters);
+		    	       String res = response.toString();
+		    	       Log.v(TAG, response.toString());
+		    	       Log.v(TAG, res.toString());
+		    	       resp = res;
+		    	}
+		    	      catch (Exception e) {
+		    	          e.printStackTrace();
+		    	          Toast.makeText(getApplicationContext(), e.getMessage(),Toast.LENGTH_SHORT).show();
+		    	      }
+		    	} 
+	    	}).start();
+	    	
+	    	try {
+
+	    	    /** wait a second to get response from server */
+	    	    Thread.sleep(1000);
+	    	    /** Inside the new thread we cannot update the main thread
+	    	    So updating the main thread outside the new thread */
+	    	    	if (null != resp && !resp.isEmpty()) {
+	    	    		 boolean check = resp.contains("Created");	    	    		  
+	    	    	       if (check == true) {
+	    	    	    	   Toast.makeText(getApplicationContext(), "New Tasklist successfully created",Toast.LENGTH_LONG).show();
+	    	    	    	   Intent it = new Intent(getApplicationContext(),TasklistActivity.class);
+	    	    	    	   startActivity(it);
+	    	    	       } 
+	    	    	}
+	    	    	
+	    	    	else {
+	    	    			Toast.makeText(getApplicationContext(), "something went wrong, check your connection",Toast.LENGTH_SHORT).show();
+	    	    	} 
+	    	            
+	    	   
+	    	    } catch (Exception e) {
+	    	    	
+	    	 }
+	    }
+	  };
 	  
-	  }
- 
- public void showListview(View view) 
- {
-	 Intent i = new Intent(getApplicationContext(),ListViewActivity.class);
-	 startActivity(i);
- }
-   
- OnClickListener viewClickListener
- = new OnClickListener(){
+  public void getTasks() throws NullPointerException {
+  	new Thread(new Runnable(){
+		
+    	@Override
+    	public void run() {
 
- @Override
- public void onClick(View v) {
-  // TODO Auto-generated method stub
-  showPopupMenu(v);
- }
-  
- };
+    		
+    		ArrayList<NameValuePair> postParameters = new ArrayList<NameValuePair>();
+    		postParameters.add(new BasicNameValuePair("username","pipi"));
+    		postParameters.add(new BasicNameValuePair("password","qqq"));
+    		postParameters.add(new BasicNameValuePair("taskname",taskName.getText().toString()));
+    	    postParameters.add(new BasicNameValuePair("tasklistid","200006"));
+    	    
+    	    String response = null;
+    	      try {
+    	       response = SimpleHttpClient.executeHttpPost("http://www.iwi.hs-karlsruhe.de/eb03/getTasks", postParameters);
+    	       String res = response.toString();
+    	       Log.v(TAG, response.toString());
+    	       Log.v(TAG, res.toString());
+    	       resp = res;
+    	}
+    	      catch (Exception e) {
+    	          e.printStackTrace();
+    	          Toast.makeText(getApplicationContext(), e.getMessage(),Toast.LENGTH_SHORT).show();
+    	      }
+    	} 
+	}).start();
+	
+	try {
 
- private void showPopupMenu(View v){
-	   PopupMenu popupMenu = new PopupMenu(TaskActivity.this, v);
-	      popupMenu.getMenuInflater().inflate(R.menu.main_popup, popupMenu.getMenu());
+	    /** wait a second to get response from server */
+	    Thread.sleep(1000);
 	    
-	      popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
-	   
-	   @Override
-	   public boolean onMenuItemClick(MenuItem item) {
-	    Toast.makeText(TaskActivity.this,
-	      item.toString(),
-	      Toast.LENGTH_LONG).show();
-	    return true;
-	   }
-	  });
+	    Log.v(TAG, resp.toString());
 	    
-	      popupMenu.show();
-	  }
- 
- @Override
- public void onBackPressed() {
-     Toast.makeText(getBaseContext(),"Hey digga, don't forget to save your task!!",Toast.LENGTH_LONG).show();
-     finish();
-     return;
- }  
- 
+	    Gson gson = new Gson();
+	    
+	    JSONArray jsonArray = new JSONArray(resp);
+	    
+	    
+	    for (int i = 0; i < jsonArray.length(); i++) {
+	        JSONObject jsonObject = jsonArray.getJSONObject(i);
+	        Task task = new Task();
+	        String TaskId = jsonObject.getString("id");
+	        String TaskName = jsonObject.getString("name");
+	        Integer TaskChecked = jsonObject.getInt("checked");
+	        String TaskTasklistId = jsonObject.getString("tasklist");
+	        task.setTask_id(Long.valueOf(TaskId).longValue());
+	        task.setName(TaskName);
+	        task.setChecked(TaskChecked);
+	        task.setTasklist(Long.valueOf(TaskTasklistId).longValue());
+	        allTasks.add(task);
+	        Log.i(Task.class.getName(), jsonObject.getString("name"));
+	      }
+	    
+	    for(Task t : allTasks){
+	    	adapter.add(t);
+	    }
+	    
+	 } 
+	catch (Exception e) {
+	    	e.printStackTrace();
+	 }
+
+		}
+
+	public void showTask(View view) throws NullPointerException {
+		try {
+
+		}
+
+		catch (Exception e) {
+			AlertDialog.Builder builder = new AlertDialog.Builder(this);
+			builder.setMessage(e.getMessage());
+			AlertDialog alert = builder.create();
+			alert.show();
+		}
+
+	}
+
+	public void showListview(View view) {
+		Intent i = new Intent(getApplicationContext(), ListViewActivity.class);
+		startActivity(i);
+	}
+
+	OnClickListener viewClickListener = new OnClickListener() {
+
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			showPopupMenu(v);
+		}
+
+	};
+
+	private void showPopupMenu(View v) {
+		PopupMenu popupMenu = new PopupMenu(TaskActivity.this, v);
+		popupMenu.getMenuInflater().inflate(R.menu.main_popup,
+				popupMenu.getMenu());
+
+		popupMenu
+				.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+
+					@Override
+					public boolean onMenuItemClick(MenuItem item) {
+						Toast.makeText(TaskActivity.this, item.toString(),
+								Toast.LENGTH_LONG).show();
+						return true;
+					}
+				});
+
+		popupMenu.show();
+	}
+
+	@Override
+	public void onBackPressed() {
+//		Toast.makeText(getBaseContext(),
+//				"Hey digga, don't forget to save your task!!",
+//				Toast.LENGTH_LONG).show();
+		finish();
+		return;
+	}
+
 }
- 
- 
-
--- myTasks/src/de/mytasks/domain/Task.java
+++ myTasks/src/de/mytasks/domain/Task.java
@@ -1,81 +1,141 @@
 package de.mytasks.domain;
 
-import com.j256.ormlite.field.DatabaseField;
-import com.j256.ormlite.table.DatabaseTable;
-//import com.j256.ormlite.table.DatabaseTable;
+import java.util.Date;
 
-import android.app.Activity;
-import android.os.Bundle;
+public class Task {
 
-@DatabaseTable(tableName = "task")
-public class Task extends Activity{
-	
-	@DatabaseField(generatedId = true)
-	private Long task_id;
-	
-	@DatabaseField
-	private String name;
-	
-	@DatabaseField
-	private String description;
-	
-	@DatabaseField
-	private Boolean value;
-	
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-	}
-
-	public Task() {
-
-	}
-
-	public Task(String name, String description, Boolean value) {
-
+	public Task(String name, int checked) {
+		super();
 		this.name = name;
-		this.description = description;
-		this.value = value;
+		this.checked = checked;
+	}
+	
+	public Task() {
+		// TODO Auto-generated constructor stub
 	}
 
+	private Long id;			// TASK_ID
+	private String name;		// T_NAME
+	private int checked;	// CHECKED
+	private Long tasklist;
+	private Date creation;		// CREATED_ON
+	private Date updated;		// UPDATED_ON
+	private String updatedBy;	// UPDATED_BY
+	
+	public int getChecked() {
+		return checked;
+	}
+
+	public void setChecked(int checked) {
+		this.checked = checked;
+	}
+
+	public Long getTasklist() {
+		return tasklist;
+	}
+
+	public void setTasklist(Long tasklist) {
+		this.tasklist = tasklist;
+	}
+	
 	public Long getTask_id() {
-		return task_id;
+		return id;
 	}
-
 	public void setTask_id(Long task_id) {
-		this.task_id = task_id;
+		this.id = task_id;
 	}
-
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
-	public String getDescription() {
-		return description;
+	public String getUpdated_by() {
+		return updatedBy;
 	}
-
-	public void setDescription(String description) {
-		this.description = description;
+	public void setUpdated_by(String updated_by) {
+		this.updatedBy = updated_by;
 	}
-
-	public Boolean getValue() {
-		return value;
+	public Date getCreation() {
+		return creation;
 	}
-
-	public void setValue(Boolean value) {
-		this.value = value;
+	public void setCreation(Date creation) {
+		this.creation = creation;
+	}
+	public Date getUpdated() {
+		return updated;
+	}
+	public void setUpdated(Date updated) {
+		this.updated = updated;
 	}
 
 	@Override
-	public String toString() {
-		return "Task [task_id=" + task_id + ", name=" + name + ", description="
-				+ description + ", value=" + value + "]";
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + checked;
+		result = prime * result
+				+ ((creation == null) ? 0 : creation.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result
+				+ ((tasklist == null) ? 0 : tasklist.hashCode());
+		result = prime * result + ((updated == null) ? 0 : updated.hashCode());
+		result = prime * result
+				+ ((updatedBy == null) ? 0 : updatedBy.hashCode());
+		return result;
 	}
-	
-	
-
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Task other = (Task) obj;
+		if (checked != other.checked)
+			return false;
+		if (creation == null) {
+			if (other.creation != null)
+				return false;
+		} else if (!creation.equals(other.creation))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (tasklist == null) {
+			if (other.tasklist != null)
+				return false;
+		} else if (!tasklist.equals(other.tasklist))
+			return false;
+		if (updated == null) {
+			if (other.updated != null)
+				return false;
+		} else if (!updated.equals(other.updated))
+			return false;
+		if (updatedBy == null) {
+			if (other.updatedBy != null)
+				return false;
+		} else if (!updatedBy.equals(other.updatedBy))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Task [id=" + id + ", name=" + name + ", checked=" + checked
+				+ ", creation=" + creation + ", updated=" + updated
+				+ ", updatedBy=" + updatedBy + "]";
+	}
+//	@Override
+//	public String toString() {
+//		return name;
+//	}
 }
