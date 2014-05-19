56954e721d145f98db45952528ba204332931074
Paul <filip.schneider1995@gmail.com>
2014-05-19 14:19:31 +0200

    test tag

--- mytasksService/src/de/util/Util.java
+++ mytasksService/src/de/util/Util.java
@@ -6,7 +6,7 @@ public class Util {
 public static final String CON = "jdbc:oracle:thin:@//iwi-w-vm-dbo.hs-karlsruhe.de:1521/oracledbwi.hs-karlsruhe.de";
 public static final String USER = "eBS14Db03";
 public static final String PW = "eBs14Db";
-
+//
 //public static final String CON = "jdbc:oracle:thin:@localhost:1521:XE";
 //public static final String USER = "test";
 //public static final String PW = "123";
