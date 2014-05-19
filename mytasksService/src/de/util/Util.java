77ed672f7e4f55b93db8a839f8780b2c676fe93f
Paul <filip.schneider1995@gmail.com>
2014-05-19 14:08:43 +0200

    tag test

--- mytasksService/src/de/util/Util.java
+++ mytasksService/src/de/util/Util.java
@@ -6,13 +6,13 @@ public class Util {
 public static final String CON = "jdbc:oracle:thin:@//iwi-w-vm-dbo.hs-karlsruhe.de:1521/oracledbwi.hs-karlsruhe.de";
 public static final String USER = "eBS14Db03";
 public static final String PW = "eBs14Db";
-//
+
 //public static final String CON = "jdbc:oracle:thin:@localhost:1521:XE";
 //public static final String USER = "test";
 //public static final String PW = "123";
 
 //SQL
 
-public static final String SQLAUTH = "SELECT name, password FROM users where name=? and password=?";
+public static final String SQLAUTH = "SELECT * FROM users where name=? and password=?";
 	
 }
