10b4342eb1348c80d1b1ad5ea54f525be6ed9502
Jürgen Biedron <biju1012@hs-karlsruhe.de>
2014-05-21 15:25:47 +0200

    * Anpassung im Session Manager * In der LogonActivity wird die JSON Response bestehend aus id, name und email abgefangen und in den sharedPreferences gespeichert.

--- myTasks/src/de/mytasks/activities/LogonActivity.java
+++ myTasks/src/de/mytasks/activities/LogonActivity.java
@@ -1,17 +1,24 @@
 package de.mytasks.activities;
- 
+
 import java.util.ArrayList;
 
 import org.apache.http.NameValuePair;
 import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import com.google.gson.Gson;
 
 import de.mytasks.R;
+import de.mytasks.domain.Tasklist;
+import de.mytasks.domain.User;
 import de.mytasks.service.SessionManager;
 import de.mytasks.service.SimpleHttpClient;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -20,97 +27,129 @@ import android.widget.Toast;
 
 @SuppressLint("NewApi")
 public class LogonActivity extends Activity {
-private EditText username = null;
-private EditText password = null;
-private Button login;
-private Button register;
-private String resp;
-private String errorMsg;
-TextView error;
+	private EditText username = null;
+	private EditText password = null;
+	private Button login;
+	private Button register;
+	private String resp;
+	private String errorMsg;
+	private static final String TAG = "LogonActivity";
+//	private User user;
+	TextView error;
 
-//Session Manager Class
-SessionManager session;
+	// Session Manager Class
+	SessionManager session;
 
- /** Called when the activity is first created. */
- @Override
- public void onCreate(Bundle savedInstanceState) {
-  super.onCreate(savedInstanceState);
-  setContentView(R.layout.activity_login);
-  
-  //Session Manager
-  session = new SessionManager(getApplicationContext());    
-  
-  username = (EditText) findViewById(R.id.loginViewUsernameInput);
-  password = (EditText) findViewById(R.id.loginViewPasswordInput);
-  login = (Button) findViewById(R.id.loginViewLoginButton);
-  register = (Button) findViewById(R.id.loginViewRegisterButton);
-  error = (TextView) findViewById(R.id.error);
-  
-  Toast.makeText(getApplicationContext(), "User Login Status: " + session.isLoggedIn(), Toast.LENGTH_LONG).show();
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_login);
 
-  login.setOnClickListener(new View.OnClickListener() {
+		// Session Manager
+		session = new SessionManager(getApplicationContext());
 
-   @Override
-   public void onClick(View v) {
-    /** According with the new StrictGuard policy,  running long tasks on the Main UI thread is not possible
-    So creating new thread to create and execute http operations */
-    new Thread(new Runnable() {
+		username = (EditText) findViewById(R.id.loginViewUsernameInput);
+		password = (EditText) findViewById(R.id.loginViewPasswordInput);
+		login = (Button) findViewById(R.id.loginViewLoginButton);
+		register = (Button) findViewById(R.id.loginViewRegisterButton);
+		error = (TextView) findViewById(R.id.error);
 
-     @Override
-     public void run() {
-      ArrayList<NameValuePair> postParameters = new ArrayList<NameValuePair>();
-      postParameters.add(new BasicNameValuePair("username",username.getText().toString()));
-      postParameters.add(new BasicNameValuePair("password",password.getText().toString()));
+		// Toast.makeText(getApplicationContext(), "User Login Status: " +
+		// session.isLoggedIn(), Toast.LENGTH_LONG).show();
 
-      String response = null;
-      try {
-//       response = SimpleHttpClient.executeHttpPost("http://10.0.2.2:8080/mytasksLogin/show", postParameters);
-//    	  response = SimpleHttpClient.executeHttpPost("http://iwi-w-eb03:8080/mytasksService/login", postParameters);
-    	  response = SimpleHttpClient.executeHttpPost("http://www.iwi.hs-karlsruhe.de/eb03/login", postParameters);
-       String res = response.toString();
-       resp = res;
-             
+		login.setOnClickListener(new View.OnClickListener() {
 
-      } catch (Exception e) {
-       e.printStackTrace();
-       errorMsg = e.getMessage();
-      }
-     }
+			@Override
+			public void onClick(View v) {
+				/**
+				 * According with the new StrictGuard policy, running long tasks
+				 * on the Main UI thread is not possible So creating new thread
+				 * to create and execute http operations
+				 */
+				new Thread(new Runnable() {
 
-    }).start();
-    try {
+					@Override
+					public void run() {
+						ArrayList<NameValuePair> postParameters = new ArrayList<NameValuePair>();
+						postParameters.add(new BasicNameValuePair("username",
+								username.getText().toString()));
+						postParameters.add(new BasicNameValuePair("password",
+								password.getText().toString()));
 
-    /** wait a second to get response from server */
-    Thread.sleep(1000);
-    /** Inside the new thread we cannot update the main thread
-    So updating the main thread outside the new thread */
-    boolean check = resp.contains("Error");
-    	if (check != true) {
-    				session.createLoginSession(username.getText().toString());
-    	    	    Intent it = new Intent(getApplicationContext(),TasklistActivity.class);
-    	     	 	startActivity(it);
-    	     	 	finish();
-    	}
-    	
-    	else {
-    		//if (null != errorMsg && !errorMsg.isEmpty()) {
-    			   Toast.makeText(getApplicationContext(), "Wrong credential! Please try again!",Toast.LENGTH_LONG).show();
-//    	   	       error.setText("Wrong credential! Please try again!");
-//    	   	       Thread.sleep(1000);
-//    	   	       error.setText("");
-    	    	 // }
-    	} 
-            
-   
-    } catch (Exception e) {
-     error.setText(e.getMessage());
-    }
-   }
-  });
- }
- public void register(View view) {
+						String response = null;
+						try {
+							// response =
+							// SimpleHttpClient.executeHttpPost("http://10.0.2.2:8080/mytasksLogin/show",
+							// postParameters);
+							// response =
+							// SimpleHttpClient.executeHttpPost("http://iwi-w-eb03:8080/mytasksService/login",
+							// postParameters);
+							response = SimpleHttpClient
+									.executeHttpPost(
+											"http://www.iwi.hs-karlsruhe.de/eb03/login",
+											postParameters);
+							String res = response.toString();
+							resp = res;
+
+						} catch (Exception e) {
+							e.printStackTrace();
+							errorMsg = e.getMessage();
+						}
+					}
+
+				}).start();
+				try {
+
+					/** wait a second to get response from server */
+					Thread.sleep(1000);
+					/**
+					 * Inside the new thread we cannot update the main thread So
+					 * updating the main thread outside the new thread
+					 */
+
+					JSONObject jsonObject = new JSONObject(resp);
+//					user = new User();
+//					String userId = jsonObject.getString("id");
+//					String userMail = jsonObject.getString("email");
+//					String userName = jsonObject.getString("name");
+//					user.setId(Long.valueOf(userId).longValue());
+//					user.setEmail(userMail);
+//					user.setName(userName);
+					Log.v(User.class.getName(), jsonObject.getString("id"));
+					Log.v(User.class.getName(), jsonObject.getString("email"));
+					Log.v(User.class.getName(), jsonObject.getString("name"));
+//					Log.v(TAG, user.toString());
+
+					boolean check = resp.contains("Error");
+					if (check != true) {
+//						session.createLoginSession(username.getText().toString());
+//						session.createLoginSession(user.getId(),
+//								user.getName(), user.getEmail());
+						session.createLoginSession(Long.valueOf(jsonObject.getString("id")),
+								jsonObject.getString("name"), jsonObject.getString("email"));
+						Intent it = new Intent(getApplicationContext(),
+								TasklistActivity.class);
+						startActivity(it);
+						finish();
+					}
+
+					else {
+						Toast.makeText(getApplicationContext(),
+								"Wrong credential! Please try again!",
+								Toast.LENGTH_LONG).show();
+					}
+
+				} catch (Exception e) {
+					e.printStackTrace();
+					error.setText(e.getMessage());
+				}
+			}
+		});
+	}
+
+	public void register(View view) {
 		Intent intent = new Intent(this, RegisterActivity.class);
 		startActivity(intent);
 	}
 }
-
--- myTasks/src/de/mytasks/service/SessionManager.java
+++ myTasks/src/de/mytasks/service/SessionManager.java
@@ -13,128 +13,128 @@ import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 
 public class SessionManager {
-    // Shared Preferences
-    SharedPreferences pref;
-     
-    // Editor for Shared preferences
-    Editor editor;
-     
-    // Context
-    Context _context;
-     
-    // Shared pref mode
-    int PRIVATE_MODE = 0;
-     
-    // Sharedpref file name
-    private static final String PREF_NAME = "myTasksPref";
-     
-    // All Shared Preferences Keys
-    private static final String IS_LOGIN = "IsLoggedIn";
-     
-    // User name (make variable public to access from outside)
-    public static final String KEY_NAME = "username";
-     
-    // Email address (make variable public to access from outside)
-    public static final String KEY_EMAIL = "email";
-    
- // User Id (make variable public to access from outside)
-    public static final String KEY_ID = "userId";
-     
-    // Constructor
-    public SessionManager(Context context){
-        this._context = context;
-        pref = _context.getSharedPreferences(PREF_NAME, PRIVATE_MODE);
-        editor = pref.edit();
-    }
-    
-    /**
-     * Create login session
-     * */
-//    public void createLoginSession(String name, String email){
-    public void createLoginSession(String name){
-        // Storing login value as TRUE
-        editor.putBoolean(IS_LOGIN, true);
-         
-        // Storing name in pref
-        editor.putString(KEY_NAME, name);
-         
-        // Storing email in pref
-//        editor.putString(KEY_EMAIL, email);
-        
-     // Storing id in pref
-//        editor.putString(KEY_ID, id);
-         
-        // commit changes
-        editor.commit();
-    }
-    
-    /**
-     * Get stored session data
-     * */
-//    public HashMap<String, String> getUserDetails(){
-    public List<String> getUserDetails(){
-//        HashMap<String, String> user = new HashMap<String, String>();
-        List<String> user = new ArrayList<String>();
-        // user name
-//        user.put(KEY_NAME, pref.getString(KEY_NAME, null));
-        user.add(pref.getString(KEY_NAME, null));
-         
-        // user email id
-//        user.put(KEY_EMAIL, pref.getString(KEY_EMAIL, null));
-        
-     // user  id
-//      user.put(KEY_ID, pref.getString(KEY_ID, null));
-         
-        // return user
-        return user;
-    }
-    
-    /**
-     * Check login method will check user login status
-     * If false it will redirect user to login page
-     * Else won't do anything
-     * */
-    public void checkLogin(){
-        // Check login status
-        if(!this.isLoggedIn()){
-            // user is not logged in redirect him to Login Activity
-            Intent i = new Intent(_context, LogonActivity.class);
-            // Closing all the Activities
-            i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-             
-            // Add new Flag to start new Activity
-            i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-             
-            // Staring Login Activity
-            _context.startActivity(i);
-        }
-         
-    }
-    /**
-     * Clear session details
-     * */
-    public void logoutUser(){
-        // Clearing all data from Shared Preferences
-        editor.clear();
-        editor.commit();
-         
-        // After logout redirect user to Loing Activity
-        Intent i = new Intent(_context, LogonActivity.class);
-        // Closing all the Activities
-        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-         
-        // Add new Flag to start new Activity
-        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-         
-        // Staring Login Activity
-        _context.startActivity(i);
-    }
-    
-    /**
-     * Quick check for login
-     * **/
-    // Get Login State
-    public boolean isLoggedIn(){
-        return pref.getBoolean(IS_LOGIN, false);
-    }
+	// Shared Preferences
+	SharedPreferences pref;
+
+	// Editor for Shared preferences
+	Editor editor;
+
+	// Context
+	Context _context;
+
+	// Shared pref mode
+	int PRIVATE_MODE = 0;
+
+	// Sharedpref file name
+	private static final String PREF_NAME = "myTasksPref";
+
+	// All Shared Preferences Keys
+	private static final String IS_LOGIN = "IsLoggedIn";
+
+	// User name (make variable public to access from outside)
+	public static final String KEY_NAME = "username";
+
+	// Email address (make variable public to access from outside)
+	public static final String KEY_EMAIL = "email";
+
+	// User Id (make variable public to access from outside)
+	public static final String KEY_ID = "userId";
+
+	// Constructor
+	public SessionManager(Context context) {
+		this._context = context;
+		pref = _context.getSharedPreferences(PREF_NAME, PRIVATE_MODE);
+		editor = pref.edit();
+	}
+
+	/**
+	 * Create login session
+	 * */
+	// public void createLoginSession(String name, String email){
+	public void createLoginSession(Long id, String name, String email) {
+		// Storing login value as TRUE
+		editor.putBoolean(IS_LOGIN, true);
+
+		// Storing name in pref
+		editor.putString(KEY_NAME, name);
+
+		// Storing email in pref
+		editor.putString(KEY_EMAIL, email);
+
+		// Storing id in pref
+		editor.putLong(KEY_ID, id);
+
+		// commit changes
+		editor.commit();
+	}
+
+	/**
+	 * Get stored session data
+	 * */
+	// public HashMap<String, String> getUserDetails(){
+	public List<String> getUserDetails() {
+		// HashMap<String, String> user = new HashMap<String, String>();
+		List<String> user = new ArrayList<String>();
+		// user name
+		// user.put(KEY_NAME, pref.getString(KEY_NAME, null));
+		user.add(pref.getString(KEY_NAME, null));
+
+		// user email id
+		user.add(pref.getString(KEY_EMAIL, null));
+
+		// user id
+		user.add(pref.getString(KEY_ID, null));
+
+		// return user
+		return user;
+	}
+
+	/**
+	 * Check login method will check user login status If false it will redirect
+	 * user to login page Else won't do anything
+	 * */
+	public void checkLogin() {
+		// Check login status
+		if (!this.isLoggedIn()) {
+			// user is not logged in redirect him to Login Activity
+			Intent i = new Intent(_context, LogonActivity.class);
+			// Closing all the Activities
+			i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+			// Add new Flag to start new Activity
+			i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+			// Staring Login Activity
+			_context.startActivity(i);
+		}
+
+	}
+
+	/**
+	 * Clear session details
+	 * */
+	public void logoutUser() {
+		// Clearing all data from Shared Preferences
+		editor.clear();
+		editor.commit();
+
+		// After logout redirect user to Loing Activity
+		Intent i = new Intent(_context, LogonActivity.class);
+		// Closing all the Activities
+		i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+		// Add new Flag to start new Activity
+		i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+		// Staring Login Activity
+		_context.startActivity(i);
+	}
+
+	/**
+	 * Quick check for login
+	 * **/
+	// Get Login State
+	public boolean isLoggedIn() {
+		return pref.getBoolean(IS_LOGIN, false);
+	}
 }
